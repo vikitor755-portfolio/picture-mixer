50e0bc6c866163aea3c756b034da97434a3d53f4
Viktor Lazarov <vikitor755@gmail.com>
2022-07-20 09:23:11 -0700

    Resetting project to new setup for code quality tools.

---
+++ src/components/LoginForm/index.tsx
@@ -0,0 +1,27 @@
+import React, { FC } from 'react';
+
+const LoginForm: FC = () => {
+
+    return <>
+        
+        <h1>LoginForm</h1>
+        <form>
+            <div>
+                <label htmlFor="email"></label>
+                <input type="email" name="email" id="emailInput" />
+            </div>
+
+            <div>
+                <label htmlFor="password"></label>
+                <input type="password" name="password" id="passwordInput" />
+            </div>
+
+            <button>
+                REGISTER
+            </button>
+        </form>
+    </>
+    
+};
+
+export default LoginForm;
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import LoginForm from '../../components/LoginForm';
 
 const Login = () => {
-    return <h1>Login me timbers</h1>;
+    return <>
+        <h1>Login me timbers</h1>
+        <LoginForm />
+    </>
 };
 export default Login;
