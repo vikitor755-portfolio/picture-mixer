5a233a5932fd9ed4aad83b25dddf433af015d36a
Viktor Lazarov <vikitor755@gmail.com>
2022-07-19 17:11:28 -0700

    Test code quality tools.

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-              <p>
-  Edit <code>src/App.tsx</code> and save to reload.
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
- className="App-link"
+          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
